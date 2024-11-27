374f46e38192b43763ddf9f7f2e8c04f15105c2b
sunat <sunat@DESKTOP-LD2DID4.localdomain>
2024-11-28 01:41:39 +0500

    1

---
+++ obu/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/ssssunat/tolling/types"
+)
+
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
+
+var senInterval = time.Second
+
+func sendOBUData(conn *websocket.Conn, data types.OBUData) error {
+	return conn.WriteJSON(data)
+}
+
+func genCoord() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+func main() {
+	obuIDS := generateOBUIDS(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := types.OBUData{
+				OBUID: obuIDS[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+		}
+		time.Sleep(senInterval)
+	}
+}
+
+func generateOBUIDS(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
